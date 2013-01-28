0a53d7308ec21856bddc01eaba8c2cbb986db326
Tatsuro Ueda <ueda_tatsuro@yahoo.co.jp>
2013-01-28 16:44:54 +0900

    'hoge'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P130128CanvasExperiment2::Application.config.secret_token = '94a9f5edab4cc71a356b5b446526bbecfad354abc27d1e9b6699487360b464387f7c32ec48291297988b208edc634ec6a990f56c318214e1b909e22251f10a1b'
