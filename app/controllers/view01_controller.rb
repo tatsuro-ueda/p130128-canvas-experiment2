416f7b29455f03eac041008a3f7bfbca323ccb5f
Tatsuro Ueda <ueda_tatsuro@yahoo.co.jp>
2013-02-01 13:59:40 +0900

    added basic authentication

--- app/controllers/view01_controller.rb
+++ app/controllers/view01_controller.rb
@@ -1,4 +1,6 @@
 class View01Controller < ApplicationController
+  #http_basic_authenticate_with :name => "geekz", :password => "pass"
+  http_basic_authenticate_with name: "todo", password: "todo"
   def index
   end
 end
