db9b20cae23b1676848ec439726ade1b6988001a
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2016-06-14 15:09:52 +0900

    ➖Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "todo", password: "todo"
-  protect_from_forgery
+#  http_basic_authenticate_with name: "todo", password: "todo"
+#  protect_from_forgery
 end
